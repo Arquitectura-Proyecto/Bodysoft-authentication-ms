649b65e967494ef385db5b46fecf69c23cdec82e
juan <jpbmdev@gmail.com>
2020-04-11 11:42:05 -0500

    small fixes

--- controller/requestHandler.go
+++ controller/requestHandler.go
@@ -17,10 +17,11 @@ func HandleRequest() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", apiStatus)
 	myRouter.HandleFunc("/create-user", createUserController).Methods("POST")
-	myRouter.HandleFunc("/authentication/{email}/{password}", authenticationController).Methods("GET")
-	myRouter.HandleFunc("/change-password", chagePasswordController).Methods("PUT")
 	myRouter.HandleFunc("/get-types", getTypesController).Methods("GET")
 	myRouter.HandleFunc("/recover-password/{email}", recoverPasswordWithEmail).Methods("GET")
+	myRouter.HandleFunc("/authentication/{email}/{password}", authenticationController).Methods("GET")
+	myRouter.HandleFunc("/change-password", chagePasswordController).Methods("PUT")
+
 	fmt.Println("Port 4000 is listening")
 	log.Fatal(http.ListenAndServe(":4000", myRouter))
 }
